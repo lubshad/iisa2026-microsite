e40d7c2a3604c9c6566f3318dd266bfbf345b43e
lubshad <lubshad4u4@gmail.com>
2026-04-06 18:37:08 +0530

    Remove basePath since a custom domain is being used

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/iisa2026-microsite" : "",
   images: {
     unoptimized: true,
     remotePatterns: [
